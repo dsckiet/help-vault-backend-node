fd859581060acef412657e04386891df61a230ca
NishantJawla13 <nishantjawla12225@gmail.com>
2021-10-22 21:42:32 +0530

    Server started, added code formmater

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(8081, () => {
+	console.log("🚀 Api Running at http://localhost:8081");
+});
+
+app.get("/", (req, res) => {
+	return res.status(200).json({
+		msg: "Welcome to the home route!"
+	});
+});
