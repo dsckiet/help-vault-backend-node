13b90f8d8e9c2c8f09278707e7a496a87e00471d
NishantJawla13 <nishantjawla12225@gmail.com>
2021-10-23 12:29:17 +0530

    added wokflow

--- test/appTest.js
+++ test/appTest.js
@@ -7,6 +7,7 @@ const runExpressServer = require("../app")
 describe('App', () => {
     //ToDO: Write Tests
     // describe('/', function() {
+    // This is not a async request but when we will write we can perform testing here
     //   it('Should return a response with status code 200', done =>  {
     //     chai.request(runExpressServer).get("/").end((err,response) => {
     //        console.log(response)
