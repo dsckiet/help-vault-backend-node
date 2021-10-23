3fc48b6daa80bc982f45221d112278ec1c9a2f51
NishantJawla13 <nishantjawla12225@gmail.com>
2021-10-23 13:40:09 +0530

    made  changes to improve code quality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require("./utility/logger/logger")
 const status = require("./utility/statusCodes")
 const app = express();
 require("./config/dbConnection");
+
 app.get("/", (req, res) => {
 	logger.info("Get request at home route")
 	return res.status(status.OK).json({
