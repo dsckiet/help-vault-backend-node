062a71042ace3bfd8664eb5b6710661907487ca5
NishantJawla13 <nishantjawla12225@gmail.com>
2021-12-05 23:13:17 +0530

    created job schmema

---
+++ src/models/job.js
@@ -0,0 +1,39 @@
+const Schema = require("mongoose").Schema;
+
+const jobSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    volunteerRequired: {
+        type: Number,
+        required: true,
+        trim: true,
+    },
+    location: {
+        type: {
+            type: String, 
+            enum: ['Point'],
+            required: true
+          },
+          coordinates: {
+            type: [Number],
+            required: true
+          }
+    },
+    date: {
+        type: String,
+        required: true
+    }
+},
+{
+    timestamps: true
+});
+
+module.exports = require("mongoose").model("Job", jobSchema);
